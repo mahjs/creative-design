5629777efb1bb393f9bec5eb49b1cdf1e053cb91
Mahdi Iranshai <mahjsdev@gmail.com>
2022-10-25 21:21:42 +0330

    First Commit for the floating-responsive appv 1.23

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ document.querySelectorAll(".story-btn").forEach((btn) => {
 const scrollBtn = document.querySelector(".scroll-up-btn");
 let showScrollBtn = false;
 document.addEventListener("scroll", () => {
-  if (!showScrollBtn && window.scrollY > 150) {
+  if (!showScrollBtn && window.scrollY > window.innerHeight / 2) {
     scrollBtn?.classList.add("show");
     scrollBtn?.classList.remove("hide");
     showScrollBtn = true;
-  } else if (showScrollBtn && window.scrollY <= 150) {
+  } else if (showScrollBtn && window.scrollY <= window.innerHeight / 2) {
     scrollBtn?.classList.remove("show");
     scrollBtn?.classList.add("hide");
     showScrollBtn = false;
